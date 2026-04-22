97e6f62e34a7a9ff79f1371bfe7a1e61b7f33c8f
MuhammadArham47 <mirzaarhamsanaullah0007@gmail.com>
2026-04-22 16:31:08 +0500

    adding a bootstrap js link file

--- Assignment 16/smit/src/components/Header/Navbar.jsx
+++ Assignment 16/smit/src/components/Header/Navbar.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <header>
-      <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-expand-lg bg-primary navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
             React App
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -18,40 +18,40 @@ function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
                 <Link to="/">
-                <a class="nav-link active" aria-current="page">
+                <a className="nav-link active" aria-current="page">
                   Home
                 </a>
                 </Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link to="/about">
-                <a class="nav-link">
+                <a className="nav-link">
                   About
                 </a>
                 </Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link to="/contact">
-                <a class="nav-link">
+                <a className="nav-link">
                   Contact
                 </a>
                 </Link>
               </li>
             </ul>
-            <form class="d-flex gap-2" role="search">
+            <form className="d-flex gap-2" role="search">
               <Link to="/auth/login">
-              <button class="btn btn-success">
+              <button className="btn btn-success">
                 Login
               </button>
               </Link>
               <Link to="/auth/register">
-              <button class="btn btn-info" type="submit">
+              <button className="btn btn-info" type="submit">
                 Register
               </button>
               </Link>
