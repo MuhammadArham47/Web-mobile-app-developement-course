c61854c444b44c470ce3b6f79cc8e5d3c2bf34e0
MuhammadArham47 <mirzaarhamsanaullah0007@gmail.com>
2026-05-20 00:09:20 +0500

    Protecting route

--- Assignment 18/todo/src/pages/Frontend/index.jsx
+++ Assignment 18/todo/src/pages/Frontend/index.jsx
@@ -1,17 +1,21 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
 import Home from './Home'
 import Todos from './Todos'
+import { useAuth } from '../../context/Auth'
 
 function Frontend() {
+
+  const { isAuth } = useAuth();
+
   return (
     <>
     <Header />
        <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/todos' element={<Todos />} />
+            <Route path='/todos' element={ isAuth ? <Todos /> : <Navigate to="/auth/login" replace />} />
             <Route path='*' element={<h1>404 Page Not Found!</h1>} />
        </Routes>
     <Footer />
--- Assignment 18/todo/src/pages/Routes.jsx
+++ Assignment 18/todo/src/pages/Routes.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Frontend from './Frontend'
 import Dashboard from './Dashboard'
 import Auth from './Auth'
+import { useAuth } from '../context/Auth'
 
 function Index() {
+
+  const { isAuth } = useAuth();
+
   return (
     <>
     <Routes>
         <Route path='/*' element={<Frontend />} />
         <Route path='/auth/*' element={<Auth />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/dashboard' element={ isAuth ? <Dashboard />: <Navigate to="/auth/login" replace />} />
     </Routes>
     </>
   )
