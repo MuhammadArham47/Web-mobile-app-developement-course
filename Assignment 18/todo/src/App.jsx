93a6f184d9f34449c2aee2e871ab4fa0de7d9136
MuhammadArham47 <mirzaarhamsanaullah0007@gmail.com>
2026-05-19 23:20:37 +0500

    Solving hero section problem

--- Assignment 18/todo/src/App.jsx
+++ Assignment 18/todo/src/App.jsx
@@ -8,7 +8,7 @@ import Index from './pages/Routes';
 function App() {
   return (
     <>
-      <ConfigProvider theme={{ token: { colorPrimary: "#1d3557" }, components: { Button: { controlOutline: 0, primaryShadow: "none" } } }}>
+      <ConfigProvider theme={{ token: { colorPrimary: "#1d3557" }, components: { Button: { controlOutline: 0, primaryShadow: "none" } } }}> 
         <Index />
       </ConfigProvider>
     </>
