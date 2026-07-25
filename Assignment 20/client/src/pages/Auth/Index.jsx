b10b2e7ebe98aa2b9451da6977220e110dc1a5ff
MuhammadArham47 <mirzaarhamsanaullah0007@gmail.com>
2026-07-25 20:46:39 +0500

    Auth folder register path fix

--- Assignment 20/client/src/pages/Auth/Index.jsx
+++ Assignment 20/client/src/pages/Auth/Index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Register from './register'
+import Register from './Register'
 import Login from './Login'
 import PageNotFound from '../../components/PageNotFound'
 
