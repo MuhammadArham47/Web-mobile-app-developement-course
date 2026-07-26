fad12abe2a917a549a727015b884e34aca7f29cd
MuhammadArham47 <mirzaarhamsanaullah0007@gmail.com>
2026-07-26 14:52:03 +0500

    Custom Changes

--- Assignment 20/server/server.js
+++ Assignment 20/server/server.js
@@ -35,9 +35,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-export default app;
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// app.listen(port, () => {
-//     console.log(`app listening on port ${port}`);
-// }); 
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+}); 
