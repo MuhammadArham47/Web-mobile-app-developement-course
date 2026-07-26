701824c383a42af77fa20c41eda078e28f7b5fb1
MuhammadArham47 <mirzaarhamsanaullah0007@gmail.com>
2026-07-26 14:40:38 +0500

    Custom Changes

--- Assignment 20/server/server.js
+++ Assignment 20/server/server.js
@@ -1,6 +1,16 @@
 import'dotenv/config';
-import app from "./src/app.js";
-import { connectDB } from "./src/config/db.js";
+import express from 'express';
+import cors from 'cors';
+import userRoutes from './src/routes/user.routes.js';
+import todoRoutes from './src/routes/todo.routes.js';
+import { connectDB } from './src/config/db.js';
+
+const app = express();
+
+app.use(cors({ origin: ["http://localhost:5173", "https://taskflow-pro-mern-stack.vercel.app"], credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 let isConnected = false;
 
@@ -16,9 +26,16 @@ app.use(async (req, res, next) => {
     }
   }
   next();
+}); 
+
+app.use('/auth', userRoutes);
+app.use('/todo', todoRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 });
 
-export default app
+export default app;
 // const port = process.env.PORT || 3000;
 
 // app.listen(port, () => {
